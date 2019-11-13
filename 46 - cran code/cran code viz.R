060a409c5e43a7b93bf25ba711c4e356980f041d
ekholme <eric.ekholm@gmail.com>
2019-11-12 20:27:28 -0500

    base graph complete

--- 46 - cran code/cran code viz.R
+++ 46 - cran code/cran code viz.R
@@ -12,12 +12,27 @@ cran <- cran_code %>%
   mutate(count = n()) %>%
   ungroup() %>%
   mutate(rank = dense_rank(desc(count))) %>% #this will give a ranking where 1 = the most common in the dataset, etc.
-  filter(rank <= 10 & is.finite(comment_code_ratio) & code > median(.$code) & comment_code_ratio <= 1) 
+  filter(rank <= 10 & is.finite(comment_code_ratio) & code > median(.$code) & comment_code_ratio <= 1) %>%
+  mutate(language = fct_reorder(language, comment_code_ratio, mean, .desc = TRUE))
 
 ggplot(cran, aes(x = comment_code_ratio, y = language)) +
-  geom_density_ridges() +
+  geom_density_ridges(color = "white", fill = "white", alpha = .8) +
   scale_x_continuous(
-    limits = c(0, 1)
+    limits = c(0, 1),
+    expand = c(0, 0)
+  ) +
+  labs(
+    x = "Comment:Code Ratio",
+    y = "Language",
+    title = "Comment to Code Ratio of R Packages, by Language"
+  ) +
+  theme(
+    rect = element_rect(fill = "black"),
+    text = element_text(family = "mono", color = "white"),
+    panel.background = element_rect(fill = NA),
+    axis.text = element_text(color = "white", family = "mono"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.y = element_blank()
   )
 
 
