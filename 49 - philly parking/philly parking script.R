0e4afe9913aa0578ad9a0696821863bbfc76dc42
Eric Ekholm <eric.ekholm@gmail.com>
2019-12-08 15:32:03 -0500

    finished plotting

--- 49 - philly parking/philly parking script.R
+++ 49 - philly parking/philly parking script.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(lubridate)
 library(zoo)
 library(forecast)
+library(broom)
+library(hrbrthemes)
 
 set.seed(0408)
 
@@ -23,16 +25,41 @@ keep_vios <- tickets %>%
   select(violation_desc) %>%
   as_vector()
 
-tix_ts <- tickets %>%
+tix_viz <- tickets %>%
   filter(is.element(violation_desc, keep_vios)) %>%
   count(issue_date, violation_desc) %>%
   mutate(n = replace_na(n, 0)) %>%
   group_by(violation_desc) %>%
   nest() %>%
   mutate(ts_obj = map(data, ~zoo(.$n, order.by = .$issue_date) %>% as.ts()),
-         auto_mod = map(ts_obj, ~auto.arima(.)))
-#leaving off here for now -- need to figure out how to broom this next
+         acf_mod = map(ts_obj, ~acf(., lag.max = 7, na.action = na.pass)),
+         acf_tidy = map(acf_mod, ~tidy(.))) %>%
+  unnest(acf_tidy) %>%
+  filter(lag != 0) %>%
+  ungroup() %>%
+  mutate(violation_desc = tools::toTitleCase(violation_desc) %>%
+           str_replace_all("over", "Over")) %>%
+  ggplot(aes(x = fct_relevel(as.factor(lag), as.character(c(7:1))), y = acf, fill = violation_desc,
+             color = violation_desc)) +
+  geom_col() +
+  facet_wrap(vars(violation_desc)) +
+  coord_flip() +
+  theme_ft_rc() +
+  scale_color_ft() +
+  scale_fill_ft() +
+  labs(
+    title = "Autocorrelations among Number of Parking Tickets Issued Daily",
+    subtitle = "We can see different patterns of autocorrelation among the most common ticket types issued in\nPhilly. Across all types, however, one day and one week lags have the strongest correlations",
+    y = "Correlation Coefficient",
+    x = "# of Days Lagged",
+    caption = "Data: Open Data Philly | Viz: Eric Ekholm (@ekholm_e)"
+  ) +
+  theme(
+    legend.position = "none",
+    axis.title.x = element_text(hjust = .5, size = 12),
+    axis.title.y = element_text(hjust = .5, size = 12, margin = margin(r = 10))
+  )
 
-
+ggsave(here::here("49 - philly parking/philly parking viz.jpeg"), plot = tix_viz, device = "jpeg")
 
 
