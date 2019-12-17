afa087b469293d535b4e85dfb091844ebc251f3a
Eric Ekholm <eric.ekholm@gmail.com>
2019-12-17 07:16:06 -0500

    base graph finished

--- 51 - adoptable dogs/adoptable dogs script.R
+++ 51 - adoptable dogs/adoptable dogs script.R
@@ -27,26 +27,56 @@ dog_descrips %>%
 #we see roughly 56k without special needs and 2.1k with special needs
 
 name_breed_counts <- dog_descrips %>%
+  mutate(name = str_replace_all(name, "BELLA", "Bella")) %>%
   count(breed_primary, name, , name = "breed_name_n", sort = TRUE) %>%
   right_join(dog_descrips %>%
               count(breed_primary, name = "breed_n", sort = TRUE) %>%
                top_n(3),
              by = "breed_primary") %>%
-  filter(breed_name_n > 5) %>%
+  filter(breed_name_n > 10) %>%
   select(-breed_n) %>%
   mutate(breed_primary = as_factor(breed_primary))
 
 #adding in some NA rows per each breed to put spacing between groups
 empty_bars <- 4
-empty <- data.frame(matrix(NA, empty_bars*nlevels(name_breed_counts$breed_primary), ncol(name_breed_counts)))
-colnames(empty) <- colnames(name_breed_counts)
-empty$breed_primary <- rep(levels(name_breed_counts$breed_primary), each = empty_bars)
+
+empty <- tibble(
+  breed_primary = rep(levels(name_breed_counts$breed_primary), each = empty_bars),
+  name = NA,
+  breed_name_n = NA
+)
 
 df <- bind_rows(name_breed_counts, empty) %>%
   arrange(breed_primary) %>%
   mutate(id = row_number(),
          breed_primary = as_factor(breed_primary)) %>%
   select(id, breed_primary, name, breed_name_n)
-#RESUME HERE TOMORROW
+
+df <- df %>%
+  mutate(tot_row = nrow(df),
+         angle = 90-360*(id-.5)/tot_row,
+         hjust = if_else(angle < -90, 1, 0),
+         use_angle = if_else(angle < -90, angle + 180, angle))
+
+axis_labs <- df %>%
+  group_by(breed_primary) %>%
+  summarize(start = min(id),
+            end = max(id) - empty_bars) %>%
+  mutate(title = (start+end)/2) %>%
+  ungroup()
+
+ggplot(df, aes(as_factor(id), y = breed_name_n, fill = breed_primary)) +
+  geom_col(alpha = .7) +
+  theme_void() +
+  ylim(-20, 50) +
+  theme(
+    plot.margin = unit(c(-1, -2, -1, -2), "cm"),
+    legend.position = "none"
+  ) +
+  coord_polar() +
+  geom_text(aes(x = id, y = breed_name_n + 1, label = name, hjust = 0, angle = angle), color = "black", size = 2.5, alpha = .6) +
+  #geom_segment(data = axis_labs, aes(x = start, y = -2, xend = end, yend = -2), color = "black")
+  geom_text(data = axis_labs, aes(x = title, y = -2, label = breed_primary), color = "black") #still need to play around wtih text position
+  #maybe makes sense to put it on the outside. Plus colors
 
 #check out this code for help: https://www.r-graph-gallery.com/297-circular-barplot-with-groups.html
