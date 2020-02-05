68754cf7e9a7717d06bac010d58173452dd1c0ab
Eric Ekholm <eric.ekholm@gmail.com>
2020-02-05 10:39:13 -0500

    added nfl color pal

--- 2020 - 6 - nfl attendance/nfl attendance script.R
+++ 2020 - 6 - nfl attendance/nfl attendance script.R
@@ -2,10 +2,25 @@ set.seed(0408)
 
 library(tidyverse)
 library(gghighlight)
+library(colorr) #for nfl color palette
 
 attendance <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/attendance.csv')
 standings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/standings.csv')
 games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/games.csv')
+
+teams <- c("cardinals",  "falcons",  "ravens",  "bills",  "panthers",  "bears",  "bengals",  "browns",
+           "cowboys", "broncos", "lions", "packers", "texans", "colts", "jaguars", "chiefs", "chargers", "rams",
+           "dolphins", "vikings", "patriots", "saints", "giants", "jets", "raiders", "eagles", "steelers", 
+           "niners","seahawks", "buccaneers", "titans", "redskins")
+
+color_tbl <- tibble(
+  team = teams,
+  colz = map(teams, ~nfl.colors(.))
+) %>%
+  unnest(cols = colz) %>%
+  filter(!is.element(colz, c("#000000", "#ffffff")))
+#next -- filter out black '#000000' and white and then keep distinct
+
 
 #ok, so, tentative plan is to use the standings data to make a 'master' plot with the yearly wins for all teams and then
 #to break out into small multiples below. Can also include some highlighting of interesting trends in the 'master' plot
