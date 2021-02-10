ca2caa9a0dd7339b9bf8fc9b2857b489aa70ac13
Eric Ekholm <eric.ekholm@gmail.com>
2021-02-09 20:15:43 -0500

    finish plot

--- 2021 - 7 - wealth and income/wealth_income_script.R
+++ 2021 - 7 - wealth and income/wealth_income_script.R
@@ -4,63 +4,36 @@
 
 library(tidyverse)
 library(eemisc)
-
+library(ggtext)
 
 #read in data
-lifetime_earn <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/lifetime_earn.csv')
-student_debt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/student_debt.csv')
-retirement <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/retirement.csv')
-home_owner <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/home_owner.csv')
-race_wealth <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/race_wealth.csv')
-income_time <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_time.csv')
 income_limits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_limits.csv')
-income_aggregate <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_aggregate.csv')
-income_distribution <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_distribution.csv')
-income_mean <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_mean.csv')
 
 
 
-# Retirement Plot ---------------------------------------------------------
-
-retirement %>%
-  ggplot(aes(x = year, y = retirement, color = race)) +
-  geom_line() +
-  theme_ee()
-
-
-# Home Owner --------------------------------------------------------------
-
-home_owner %>%
-  ggplot(aes(x = year, y = home_owner_pct, color = race)) +
-  geom_line() +
-  theme_ee()
+# Plot --------------------------------------------------------------------
 
 
 
-# Income Time -------------------------------------------------------------
-
-
-income_limits %>%
-  mutate(race = case_when(
-    str_detect(race, "White") ~ "White",
-    str_detect(race, "Black") ~ "Black",
-    TRUE ~ race
-  )) %>%
-  filter(dollar_type == "2019 Dollars",
-         race %in% c("Black", "Hispanic", "White"))
-
-
-####new idea : plot the ratio of 90% all race income vs 10% all race income over time to show inequality
-#could also do each race as a greyed-out subplot in the same plot
-
-income_limits %>%
+df <- income_limits %>%
   filter(dollar_type == "2019 Dollars",
          race == "All Races") %>%
   pivot_wider(names_from = income_quintile,
               values_from = income_dollars) %>%
-  mutate(disparity = `Top 5%`/Lowest) %>%
+  mutate(disparity = `Top 5%`/Lowest)
+
+#getting first and last points to add to text
+disp_first <- round(df$disparity[df$year == 1967], 2)
+disp_last <- round(df$disparity[df$year == 2019], 2)
+
+df %>%
   ggplot(aes(x = year, y = disparity)) +
   geom_line(size = 2) +
+  annotate("richtext", x = 1967, y = 5,
+           label = "In 1967, an American family<br>in the top 5% of income<br>earned **<span style='color:#660012'>6.3x</span>** more than<br>a family in the bottom 20%.", hjust = 0, label.color = NA, fill = NA) +
+  annotate("richtext", x = 2019, y = 8.3,
+           label = "In 2019, this<br>disparity was **<span style='color:#660012'>9.6x</span>**",
+           hjust = 1, label.color = NA, fill = NA) +
   scale_y_continuous(
     limits = c(0, 10),
     labels = function(x) paste0(x, "x")
@@ -71,12 +44,16 @@ income_limits %>%
   labs(
     y = "Income Disparity",
     x = NULL,
-    title = "Income Inequality Continues to Grow"
+    title = "Income Inequality Continues to Grow",
+    caption = "Data: Urban Institute & US Census | Viz: Eric Ekholm (@ekholm_e)"
   ) +
   theme_ee() +
   theme(
     panel.grid.minor = element_blank(),
-    panel.grid.major = element_line(color = "grey80")
+    #panel.grid.major = element_line(color = "grey80"),
+    axis.line = element_line(),
+    panel.grid.major = element_blank(),
+    plot.title = element_markdown(size = 25)
   )
   
   
